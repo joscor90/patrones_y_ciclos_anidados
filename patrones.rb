3961bfb7a00814ff686d96d2b873ee641f6e4498
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-08-27 14:40:33 -0400

    Added middle section to letra_i method

--- patrones.rb
+++ patrones.rb
@@ -24,9 +24,23 @@ def letra_i(n)
     #Sección superior
     print "*"*n+"\n"
 
+    #Sección media
+    (n-2).times do 
+        (n/2).times do |i|
+            print " "
+        end
+        print "*"
+        (n/2).times do |i|
+            print " "
+        end
+        print "\n"
+    end
+
     #Sección inferior
     print "*"*n+"\n"
 end
 
+letra_o(5)
+puts "\n"
 letra_i(5)
 
