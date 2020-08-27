fa368dcc0e8347b146a611be26f8b9eb84ee534c
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-08-27 14:55:14 -0400

    Added upper and lower sections to letra_z method

--- patrones.rb
+++ patrones.rb
@@ -40,7 +40,24 @@ def letra_i(n)
     print "*"*n+"\n"
 end
 
+#letra_z
+def letra_z(n)
+        #Sección superior
+        print "*"*n+"\n"
+
+        #Sección media
+        
+
+
+    
+        #Sección inferior
+        print "*"*n+"\n"
+    end
+
+
 letra_o(5)
 puts "\n"
 letra_i(5)
+puts "\n"
+letra_z(5)
 
